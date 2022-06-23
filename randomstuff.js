3bad78ad66b419a686e9b9113e75a1dde6a5adac
elwiiman <emmanuel.sanro@gmail.com>
2022-06-23 18:15:39 -0500

    Add ability to change gifs randomly

---
+++ randomstuff.js
@@ -0,0 +1,18 @@
+const getRandomNumber = (limit) => Math.floor(limit * Math.random());
+
+const getRandomFromArray = (data) => data[getRandomNumber(data.length)];
+
+const doSomething = () => {
+  const timeArray = new Array(200, 300, 150, 250, 2000, 3000, 1000, 1500);
+
+  console.log("execute something");
+  const randomNumber = getRandomNumber(7);
+  console.log("randomNumber", randomNumber);
+  cointainer.style.backgroundImage = `url(./gifs/${randomNumber}.gif)`;
+
+  clearInterval(timer);
+  timer = setInterval(doSomething, getRandomFromArray(timeArray));
+};
+
+let timer = setInterval(doSomething, 1000);
+const cointainer = document.getElementById("container");
